bd5a1d93bef4b94db30f2506a301caf2dae780dd
CodeMasters12218 <1716546@alu.murciaeduca.es>
2025-05-17 20:14:23 +0200

    Modificada tabla de usuarios para que se parezca al modelo.

---
+++ Migrations/20250517181211_AddCustomFieldsToAspNetUsers.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tradie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomFieldsToAspNetUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DeliveryFee",
+                table: "ShoppingCarts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Subtotal",
+                table: "ShoppingCarts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total",
+                table: "ShoppingCarts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "CartItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "CartItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "CartItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "CartItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastNames",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Phone",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryFee",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "Subtotal",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "ShoppingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastNames",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "password",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+        }
+    }
+}
