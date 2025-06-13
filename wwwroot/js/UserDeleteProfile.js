7d7560ee419f9171e8074768f063a41925c62348
frannjmnzz2 <franciscocastanojimenez@gmail.com>
2025-06-13 21:10:20 +0200

    He implementado la función para poder borrar tu propio usuario desde el perfil.

---
+++ wwwroot/js/UserDeleteProfile.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const deletePopUpButton = document.getElementById("DeletePopUpButton");
+    const deletePopUp = document.getElementById("DeletePopUp");
+    const cancelButton = document.getElementById("CancelButton");
+
+    deletePopUpButton.addEventListener("click", function () {
+        deletePopUp.showModal();
+    });
+
+    cancelButton.addEventListener("click", function () {
+        deletePopUp.close();
+    });
+});
